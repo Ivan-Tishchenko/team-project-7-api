8808fdfa8dd7932bb8041e1df4e61c277d2ddb0e
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-08-29 15:39:55 +0300

    delet review

--- review/deleteUserReview.js
+++ review/deleteUserReview.js
@@ -1,3 +1,10 @@
-const deleteUserReview = async (req, res, next) => {};
+const { Review } = require("../models/review");
+
+const deleteUserReview = async (req, res, next) => {
+  const review = await Review.findOneAndDelete({
+    name: req.user.name,
+  });
+  res.status(204).json(review);
+};
 
 module.exports = deleteUserReview;
