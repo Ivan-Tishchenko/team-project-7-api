4e995265342a0b7d83af20520d513af26fe8daa1
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-09-06 23:43:09 +0300

    fix google auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -35,7 +35,7 @@ passport.use(
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SICRET,
       callbackURL:
-        "http://localhost:3000/api/auth/google/callback",
+        "https://group-project-7.onrender.com/api/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       return done(null, profile);
@@ -62,66 +62,68 @@ router.get(
   "/google/callback",
   passport.authenticate("google", {
     failureRedirect:
-      "https://etopritika.github.io/team-project-7/", // Маршрут для перенаправления при неудачной аутентификации
+      "https://etopritika.github.io/team-project-7?error=google-auth", // Маршрут для перенаправления при неудачной аутентификации
   }),
   async function (req, res) {
     // Успешная аутентификация, можете выполнить дополнительные действия и перенаправить пользователя
+    try {
+      let jwt = null;
 
-    let jwt = null;
+      const currentDate = new Date();
+      const formattedDate = currentDate.toISOString();
 
-    const currentDate = new Date();
-    const formattedDate = currentDate.toISOString();
-
-    const user = await User.findOne({
-      email: req.user._json.email,
-    });
-
-    if (user) {
-      const isPasswordTrue = await compareHashPassword(
-        `${req.user._json.sub}+${req.user._json.email}`,
-        user.password
-      );
-
-      if (isPasswordTrue) {
-        jwt = createJWT({
-          _id: user._id,
-          email: user.email,
-        });
-      } else {
-        console.log("password false");
-      }
-    } else {
-      const hashPassword = await createHashPassword(
-        `${req.user._json.sub}+${req.user._json.email}`
-      );
-
-      const { _id, email } = await User.create({
-        name: req.user._json.name,
+      const user = await User.findOne({
         email: req.user._json.email,
-        password: hashPassword,
-        avatarURL: req.user._json.picture,
-        token: null,
-        createdAt: formattedDate,
-        updatedAt: formattedDate,
       });
 
-      user._id = _id;
+      if (user) {
+        const isPasswordTrue = await compareHashPassword(
+          `${req.user._json.sub}+${req.user._json.email}`,
+          user.password
+        );
 
-      jwt = createJWT({
-        _id,
-        email,
-      });
+        if (isPasswordTrue) {
+          jwt = createJWT({
+            _id: user._id,
+            email: user.email,
+          });
+        }
+      } else {
+        const hashPassword = await createHashPassword(
+          `${req.user._json.sub}+${req.user._json.email}`
+        );
+
+        const { _id, email } = await User.create({
+          name: req.user._json.name,
+          email: req.user._json.email,
+          password: hashPassword,
+          avatarURL: req.user._json.picture,
+          token: null,
+          createdAt: formattedDate,
+          updatedAt: formattedDate,
+        });
+
+        user._id = _id;
+
+        jwt = createJWT({
+          _id,
+          email,
+        });
+      }
+      await User.findOneAndUpdate(
+        { _id: user._id },
+        { token: jwt }
+      );
+
+      res.redirect(
+        jwt
+          ? `https://etopritika.github.io/team-project-7?token=${jwt}`
+          : "https://etopritika.github.io/team-project-7"
+      );
+    } catch (error) {
+      console.log(error.message);
+      res.status(error.status).json(error.message);
     }
-    await User.findOneAndUpdate(
-      { _id: user._id },
-      { token: jwt }
-    );
-
-    res.redirect(
-      jwt
-        ? `https://etopritika.github.io/team-project-7?token=${jwt}`
-        : "https://etopritika.github.io/team-project-7"
-    );
   }
 );
 
